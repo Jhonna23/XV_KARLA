9a205721c63aaf0d61f3446db2df365a70a1650d
Jhonna23 <daiter12@hotmail.com>
2023-06-16 01:00:11 -0600

    first commit

---
+++ docs/windex.php
@@ -0,0 +1,95 @@
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VV KARLA ELIZABETH</title>
+
+    <!--BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=MonteCarlo&amp;display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</head>
+
+<body style="background-image: url('http://localhost/KARLA_XV/img/back2.jpeg');
+background-repeat: no-repeat;background-size: cover;
+">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-12">
+                <!-- <h1 class="text-center">Imagen</h1> -->
+                <img src="http://localhost/KARLA_XV/img/LETRAS.png" alt="NAME" class="img-fluid mx-auto d-block" width="50%">
+
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h1 style="font-family: 'MonteCarlo', cursive; !important; color:#edd679 !important;
+        font-style: italic;
+    font-weight: 600;" class="text-center">Mis Padres</h1>
+                <span class="border-black _animate__slideInLeft animate__animated animate__slideInLeft" style="visibility: visible;width: 60%;height: 1px;border: 1px solid transparent;border-image: linear-gradient(to right, #C48F40 0%, #F9B64A 26.1659%, #FBCA7A 37.2632%, #F9B64A 47.7546%, #C48F40 67.3908%, #F9B64A 92.3143%, #FBCA7A 100%) 1;background-color: #C48F40;margin: 15px auto;display: block;"></span>
+            </div>
+        </div>
+        <div class="row mb-4">
+            <div class="col-sm-6">
+                <h4 style="font-family: 'MonteCarlo', cursive; !important; color:#edd679 !important;
+        font-style: italic;
+    font-weight: 600;" class="text-center">Lemni Marili López López</h4>
+                <span class="border-black _animate__slideInLeft animate__animated animate__slideInLeft" style="visibility: visible;width: 60%;height: 1px;border: 1px solid transparent;border-image: linear-gradient(to right, #C48F40 0%, #F9B64A 26.1659%, #FBCA7A 37.2632%, #F9B64A 47.7546%, #C48F40 67.3908%, #F9B64A 92.3143%, #FBCA7A 100%) 1;background-color: #C48F40;margin: 15px auto;display: block;"></span>
+
+            </div>
+            <div class="col-sm-6">
+                <h4 style="font-family: 'MonteCarlo', cursive; !important; color:#edd679 !important;
+        font-style: italic;
+    font-weight: 600;" class="text-center">José Javier Vázquez Solis</h4>
+                <span class="border-black _animate__slideInLeft animate__animated animate__slideInLeft" style="visibility: visible;width: 60%;height: 1px;border: 1px solid transparent;border-image: linear-gradient(to right, #C48F40 0%, #F9B64A 26.1659%, #FBCA7A 37.2632%, #F9B64A 47.7546%, #C48F40 67.3908%, #F9B64A 92.3143%, #FBCA7A 100%) 1;background-color: #C48F40;margin: 15px auto;display: block;"></span>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-11 mx-auto mt-4" style="
+    background: #1d1d1dc2;
+    height: 125px;
+    max-width: 80%;
+    border-radius: 50px 65px 114px 82px / 25px 23px 27px 32px;
+    box-shadow: -1px 9px 14px 8px rgba(0,0,0,0.75);
+    -webkit-box-shadow: -1px 9px 14px 8px rgba(0,0,0,0.75);
+    -moz-box-shadow: -1px 9px 14px 8px rgba(0,0,0,0.75);
+    ">
+                sdf
+
+
+            </div>
+
+
+        </div>
+        <div class="row">
+            <div class="col-sm-12 mt-4">
+                <span class="border-black _animate__slideInLeft animate__animated animate__slideInLeft" style="visibility: visible;/* width: 60%; */height: 1px;border: 1px solid transparent;border-image: linear-gradient(to right, #C48F40 0%, #F9B64A 26.1659%, #FBCA7A 37.2632%, #F9B64A 47.7546%, #C48F40 67.3908%, #F9B64A 92.3143%, #FBCA7A 100%) 1;background-color: #C48F40;margin: 15px auto;display: block;"></span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6" style="
+    background-color: #c5d1dd;
+">
+                    <h4 class="text-center" style="
+    background-color: background-color: #c5d1dd;
+    /* background-color: #c5d1dd; */">Salon</h4>
+                   <span class="" style="visibility: visible;/* width: 60%; */height: 1px;border: 1px solid transparent;border-image: linear-gradient(to right, #C48F40 0%, #F9B64A 26.1659%, #FBCA7A 37.2632%, #F9B64A 47.7546%, #C48F40 67.3908%, #F9B64A 92.3143%, #FBCA7A 100%) 1;background-color: #C48F40;margin: 15px auto;display: block;"></span>
+
+            </div>
+            <div class="col-md-6" style="background-color: #c5d1dd;
+">
+                
+                   <h3 class="text-center">Ceremonia</h3>
+                   <span class="" style="visibility: visible;/* width: 60%; */height: 1px;border: 1px solid transparent;border-image: linear-gradient(to right, #C48F40 0%, #F9B64A 26.1659%, #FBCA7A 37.2632%, #F9B64A 47.7546%, #C48F40 67.3908%, #F9B64A 92.3143%, #FBCA7A 100%) 1;background-color: #C48F40;margin: 15px auto;display: block;"></span>
+             </div>
+        </div>
+    </div>
+ 
+</body>
+</html>
